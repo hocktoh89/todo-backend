d25b5bd3ee8ba16b6a70d4f7a2fc13925cf79514
Thomas Yap Hock Toh <yhtthomas@gmail.com>
2021-06-05 00:48:02 +0800

    feature: complete getAll todos route

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createToDoRouter} from './todo';
+import { createToDoRouter, getToDosRouter } from './todo';
 
 const router = express.Router();
 
 router.post('/', createToDoRouter);
+router.get('/', getToDosRouter);
 
 module.exports = router
