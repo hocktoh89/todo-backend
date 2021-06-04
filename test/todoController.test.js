3de849b984cc66f3e4d7552d69fed360b5c58cb4
Thomas Yap Hock Toh <yhtthomas@gmail.com>
2021-06-04 22:46:46 +0800

    refactor: rename and rearrange todo controller test file

---
+++ test/todoController.test.js
@@ -0,0 +1,28 @@
+const createToDo = require('../src/controller/todo');
+
+const db = require('../src/db');
+
+beforeAll(async () => await db.connect());
+
+afterAll(async () => await db.closeDatabase());
+
+describe('Test ToDo Controller', () => {
+
+    describe('Test Creation', () => {
+        test('Insert ToDo to DB', async () => {
+            const { toDoId, text } = await createToDo('See Doctor');
+
+            expect(toDoId).toBeTruthy();
+            expect(text).toBe('See Doctor');
+        })
+
+        test('Throw Exception on Add Same Record Found', async () => {
+            try {
+                await createToDo('See Doctor')
+            } catch (err) {
+                expect(err.message).toBe('To do - See Doctor - exist');
+            }
+
+        })
+    });
+})
